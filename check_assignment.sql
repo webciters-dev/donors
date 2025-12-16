7d39b9b2dcb92cb17decad4fd759868b6f3a1086
webciters <webciters@gmail.com>
2025-12-16 08:05:12 +0100

    docs: reorganize documentation into categorized folders
    - Move 49 markdown files to /documentation/ folder
    - Organize into: developer-guides, deployment-procedures, analysis-reports, user-manuals
    - Keep 6 critical files in root (README, START_HERE, DO_NOT_BREAK_GUIDE, etc)
    - Update .gitignore to exclude *.ps1, *.docx, *.xlsx, builds/
    - Cleaner repository structure for team collaboration

--- check_assignment.sql
+++
@@ -1,2 +0,0 @@
--- Check if FieldReview was created for application fu41640p
-SELECT id, "applicationId", "studentId", "officerUserId", status, "taskType" FROM field_reviews WHERE "applicationId" = 'fu41640p';
